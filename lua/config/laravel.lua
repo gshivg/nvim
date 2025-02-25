dd7cb9b35195087347df638288cbc63f6d820db9
Shivansh Gupta <shivanshkush254@gmail.com>
2025-02-25 22:35:03 +0530

    updates

--- lua/config/laravel.lua
+++ lua/config/laravel.lua
@@ -1,6 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 require("conform").setup({
   formatters_by_ft = {
     blade = { "blade-formatter" },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,6 +7,7 @@ return {
       "stylua",
       "shellcheck",
       "shfmt",
+      "clangd",
       "flake8",
     },
   },
